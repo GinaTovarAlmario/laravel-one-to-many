29c352a001f8f3cfebfb1d4d11c3ca379822e990
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-06 21:32:19 +0100

    fixed style select

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -58,9 +58,8 @@
                     @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="type_id" class="form-label">Type</label>
-                    <select name="type_id" id="type_id">
-                        <option value="">Select a Type</option>
+                    <label for="type_id" class="form-label">Select a Type</label>
+                    <select name="type_id" id="type_id" class="form-control">
                         @foreach ($types as $type)
                             <option value="{{ $type->id }}">
                                 {{ $type->name }}
